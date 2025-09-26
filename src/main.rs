59720571df32612f30861a10d7e9da82ff0edf4f
Qu Hao <quhxuxm_quh@163.com>
2025-09-26 22:17:55 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
 use std::error::Error;
 
+struct Pick {
+    red_balls: Vec<u8>,
+    blue_ball: u8,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let rounds = 5;
+    let rounds = 10000000;
+    let mut chosen_rounds = vec![];
+    for _ in 1..=5 {
+        let round = rand::random_range(0..rounds);
+        chosen_rounds.push(round);
+    }
+    let mut picks = vec![];
     for _ in 1..=rounds {
-        println!("##########");
         let red_balls = generate_red_balls().await;
         let blue_ball = generate_blue_ball().await;
+        let pick = Pick {
+            red_balls,
+            blue_ball,
+        };
+        picks.push(pick);
+    }
+    chosen_rounds.into_iter().for_each(|round_index| {
+        let Pick {
+            red_balls,
+            blue_ball,
+        } = &picks[round_index];
+        println!("##########");
         println!("红球: {red_balls:?}");
         println!("蓝球: {blue_ball}");
         println!("##########");
-    }
+    });
     tokio::signal::ctrl_c().await?;
     Ok(())
 }
 
 async fn generate_red_balls() -> Vec<u8> {
-    let mut all_red_balls = vec![
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 31, 32, 33,
-    ];
+    let all_red_balls = 1..=33;
+    let mut all_red_balls = all_red_balls.collect::<Vec<u8>>();
     let mut result = vec![];
     let mut i = 0;
     while i < 6 {
@@ -33,6 +53,6 @@ async fn generate_red_balls() -> Vec<u8> {
 }
 
 async fn generate_blue_ball() -> u8 {
-    let blue_ball = rand::random_range(0..16) + 1;
+    let blue_ball = rand::random_range(1..=16);
     blue_ball
 }
