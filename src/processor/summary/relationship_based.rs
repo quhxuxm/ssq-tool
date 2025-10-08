d35c83ec17b54554344fac9c84b787ec21d5082d
Qu Hao <quhxuxm_quh@163.com>
2025-10-08 16:49:05 +0800

    init

--- src/processor/summary/relationship_based.rs
+++ src/processor/summary/relationship_based.rs
@@ -1,15 +1,16 @@
 use crate::error::Error;
 use crate::processor::context_obj::BallOccurInfo;
 use crate::processor::{
-    // RED_BALL_RELATIONSHIPS,
     Context,
     Processor,
+    // RED_BALL_RELATIONSHIPS,
     BLUE_BALL_RELATIONSHIPS,
     MOST_POSSIBLE_OCCUR_BLUE_BALLS,
     MOST_POSSIBLE_OCCUR_RED_BALLS,
 };
-use rand::{rng, Rng};
-use std::collections::HashSet;
+use itertools::Itertools;
+use rand::rng;
+use rand::seq::SliceRandom;
 use std::sync::Arc;
 use tracing::{debug, info};
 
@@ -43,12 +44,55 @@ impl Processor for RelationshipBasedProcessor {
             .ok_or(Error::ContextAttrNotExist(
                 MOST_POSSIBLE_OCCUR_RED_BALLS.name.clone(),
             ))?;
-        let mut top_n_blue_balls = most_possible_occur_blue_balls
-            .iter()
+        let mut top_n_blue_balls_by_next_index = most_possible_occur_blue_balls
+            .clone()
+            .into_iter()
+            .collect::<Vec<Arc<BallOccurInfo>>>();
+        top_n_blue_balls_by_next_index.sort_by(|v1, v2| {
+            v2.possible_next_occur_index()
+                .cmp(&v1.possible_next_occur_index())
+        });
+        let mut top_n_blue_balls_by_next_index = top_n_blue_balls_by_next_index
+            .into_iter()
             .take(5)
+            .collect::<Vec<Arc<BallOccurInfo>>>();
+        top_n_blue_balls_by_next_index.sort_by_key(|v1| v1.ball());
+
+        let mut top_n_blue_balls_by_possibility = most_possible_occur_blue_balls
+            .clone()
+            .into_iter()
+            .collect::<Vec<Arc<BallOccurInfo>>>();
+        top_n_blue_balls_by_possibility.sort_by(|v1, v2| {
+            v2.average_occur_possibility()
+                .total_cmp(&v1.average_occur_possibility())
+        });
+        let mut top_n_blue_balls_by_possibility = top_n_blue_balls_by_possibility
+            .into_iter()
+            .take(5)
+            .collect::<Vec<Arc<BallOccurInfo>>>();
+        top_n_blue_balls_by_possibility.sort_by_key(|v1| v1.ball());
+
+        let mut top_n_blue_balls = top_n_blue_balls_by_next_index;
+        top_n_blue_balls.extend(top_n_blue_balls_by_possibility);
+
+        let mut top_n_blue_balls = top_n_blue_balls
+            .iter()
+            .unique_by(|blue_ball| blue_ball.ball())
             .cloned()
             .collect::<Vec<Arc<BallOccurInfo>>>();
-        top_n_blue_balls.sort_by_key(|v1| v1.ball());
+        let mut rng = rng();
+        top_n_blue_balls.shuffle(&mut rng);
+        let top_n_blue_balls_for_log = top_n_blue_balls
+            .iter()
+            .map(|blue_ball| blue_ball.ball())
+            .collect::<Vec<usize>>();
+        debug!("根据出现可能性和下次出现索引生成待选蓝球：{top_n_blue_balls_for_log:?}",);
+        let mut top_n_blue_balls = top_n_blue_balls
+            .into_iter()
+            .take(5)
+            .collect::<Vec<Arc<BallOccurInfo>>>();
+        top_n_blue_balls.sort_by_key(|blue_ball| blue_ball.ball());
+
         top_n_blue_balls.iter().for_each(|blue_ball| {
             let blue_ball_relationship = blue_ball_relationships.get(&blue_ball.ball());
             let blue_ball_relationship = match blue_ball_relationship {
@@ -72,27 +116,19 @@ impl Processor for RelationshipBasedProcessor {
                     .map(|occur| occur.ball())
                     .take(6),
             );
-            let mut red_balls_set = HashSet::new();
-            red_balls.iter().for_each(|ball| {
-                red_balls_set.insert(*ball);
-            });
-            let mut red_balls = red_balls_set.into_iter().collect::<Vec<usize>>();
+
+            let mut red_balls = red_balls.into_iter().unique().collect::<Vec<usize>>();
             red_balls.sort();
             debug!("根据关联关系生成待选红球：{:?}", red_balls);
-            let mut chosen_red_balls = Vec::new();
-            loop {
-                let index = rng().random_range(0..red_balls.len());
-                let red_ball = red_balls.remove(index);
-                chosen_red_balls.push(red_ball);
-                if chosen_red_balls.len() == 6 {
-                    break;
-                }
-            }
-            chosen_red_balls.sort();
+
+            red_balls.shuffle(&mut rng);
+            let mut red_balls = red_balls.into_iter().take(6).collect::<Vec<usize>>();
+
+            red_balls.sort();
             info!(
                 "根据关联关系选出，蓝球：{}，红球：{:?}",
                 blue_ball.ball(),
-                chosen_red_balls
+                red_balls
             );
         });
         Ok(())
