cbc0e86f42eb18a7e1be238247961a12a8ccc54c
Qu Hao <quhxuxm_quh@163.com>
2025-10-22 18:24:32 +0800

    init

--- processor/src/generate_normalize_data.rs
+++ processor/src/generate_normalize_data.rs
@@ -1,6 +1,7 @@
 use crate::context::ProcessorContext;
 use crate::error::Error;
-use crate::{BALL_OCCURRENCE, Processor};
+use crate::{BALL_OCCURRENCE, BALL_RELATIONSHIP_FP, Processor};
+use itertools::Itertools;
 use ssq_tool_domain::Ball;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -26,6 +27,9 @@ impl Processor for GenerateNormalizeDataProcessor {
         let ball_occurrence = context
             .get_attribute(&BALL_OCCURRENCE)
             .ok_or(Error::ContextAttrNotExist(BALL_OCCURRENCE.to_string()))?;
+        let ball_occurrence_fp = context
+            .get_attribute(&BALL_RELATIONSHIP_FP)
+            .ok_or(Error::ContextAttrNotExist(BALL_RELATIONSHIP_FP.to_string()))?;
         if std::fs::remove_file(&self.file_path).is_err() {
             warn!("没有旧文件：{:?}", self.file_path)
         };
@@ -36,9 +40,16 @@ impl Processor for GenerateNormalizeDataProcessor {
         context.get_prize_records().iter().try_for_each(|record| {
             let blue_ball=Ball::Blue(record.blue_ball);
             let blue_ball_occurrence = ball_occurrence.get(&blue_ball).unwrap();
+            let top_related_red_balls=match ball_occurrence_fp.get(&record.blue_ball){
+                Some(fp_result) => {
+                   fp_result.frequent_patterns().iter().sorted_by_key(|pattern| pattern.1).flat_map(|pattern|{&pattern.0}).copied().unique().take(6).sorted().collect()
+                },
+                None => vec![],
+            };
+
             writeln!(
                 output_file,
-                "{}，期号：{}，星期：{}，蓝球：{}，红球：{:?}，总中奖注数：{}，总销售注数：{}，蓝球平均出现间隔：{}，蓝球基于平均出现间隔的出现次数：{}，蓝球出现的实际次数：{}",
+                "{}，期号：{}，星期：{}，蓝球：{}，红球：{:?}，总中奖注数：{}，总销售注数：{}，蓝球平均出现间隔：{}，蓝球预期出现次数：{}，蓝球实际出现次数：{}，推测关联红球：{:?}",
                 record.date,
                 record.code,
                 record.day,
@@ -48,9 +59,9 @@ impl Processor for GenerateNormalizeDataProcessor {
                 record.total_tickets,
                 blue_ball_occurrence.average_occur_interval(),
                 blue_ball_occurrence.occurrence_count_by_average_interval(),
-                blue_ball_occurrence.occurrence_count_by_official_data()
+                blue_ball_occurrence.occurrence_count_by_official_data(),
+                top_related_red_balls
             )?;
-
             Result::<(), Error>::Ok(())
         })?;
         Ok(())
